bfc82996b55c5e4a613e9f2074dbf0afb2c68235
Christoffel <24838075+hiltermann@users.noreply.github.com>
2025-07-17 16:20:22 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -77,29 +77,13 @@ class Configuration:
         self.avg_shot_duration_seconds = 2
         self.dynamic_cutoff_ms = 3000
 
-        # set model
-        self.llm_name = "gemini-1.5-pro-002"
-        self.video_size_limit_mb = 50
-        self.max_output_tokens = 8192
-        self.temperature = 1
-        self.top_p = 0.95
-        self.top_k = 32
-
-
     def set_parameters(self,
         project_id: str,
-        project_zone: str,
-        bucket_name: str,
         knowledge_graph_api_key: str,
-        bigquery_dataset:str,
-        bigquery_table:str,
-        assessment_file: str,
         spreadsheet_id: str,
         input_sheet: str,
         output_sheet: str,
         use_annotations: bool,
-        use_llms: bool,
-        verbose: bool,
         local_path: str
     ) -> None:
         """Set the required parameters for ABCD to run.
@@ -108,64 +92,21 @@ class Configuration:
 
         Args:
           project_id: Google Cloud Project ID
-          project_zone: Google Cloud Project zone (us-central1 if None)
-          bucket_name: Google Cloud Storage Bucket name (not uri)
           knowledge_graph_api_key: Google Cloud API Key (limit this)
-          bigquery_dataset: name of dataset in BigQuery.
-          bigquery_table: name of table to append results to in BigQuery.
-          assessment_file: If present, results will be written to the file path.
           spreadsheet_id: The id of the Google Spreadsheet.
           input_sheet: Input sheet name.
           output_sheet: Output sheet name.
           use_annotations: Use video annotation AI.
-          use_llms: Use LLM AI.
-          verbose: Turn on extra debug and execution prints.
           local_path: Local path in colab to store annotation files.
         """
         self.project_id = project_id
-        self.project_zone = project_zone or "us-central1"
-        self.bucket_name = bucket_name
         self.knowledge_graph_api_key = knowledge_graph_api_key.strip()
-        self.bq_dataset_name = bigquery_dataset
-        self.bq_table_name = bigquery_table
-        self.assessment_file = assessment_file
         self.spreadsheet_id = spreadsheet_id
         self.input_sheet = input_sheet
         self.output_sheet = output_sheet
         self.use_annotations = use_annotations
-        self.use_llms = use_llms
-        self.verbose = verbose
         self.local_path = local_path
         self.annotation_path = f"gs://{bucket_name}/ABCD/"
-
-
-    def set_videos(self, video_uris: list) -> None:
-        """Set the videos that will be processed.
-
-          Having a separate method for this allows multiple runs.
-          We accept a string in case someone passes only one video.
-
-          Args:
-            video_uris: a list of Google Cloud Storage URIs for videos or paths.
-        """
-        if isinstance(video_uris, str):
-            self.video_uris = [v.strip() for v in video_uris.split(",")]
-        elif isinstance(video_uris, (list, tuple)):
-            self.video_uris = video_uris
-        else:
-            self.video_uris = [video_uris]
-
-
-    def set_gcs_drive_mapping(self, gcs_drive_mapping: dict[str,str]) -> None:
-        """Set the mapping between drive and gcs for videos that will be processed.
-
-          Having a separate method for this allows multiple runs.
-
-          Args:
-            gcs_drive_mapping: a dict of Google Cloud Storage URIs and drive urls.
-        """
-        self.gcs_drive_mapping = gcs_drive_mapping
-
 
     def set_brand_details(self,
         brand_name: str,
@@ -188,7 +129,6 @@ class Configuration:
         self.branded_products = [t.strip() for t in products.split(",")]
         self.branded_products_categories = [t.strip() for t in products_categories.split(",")]
         self.branded_call_to_actions = [t.strip() for t in call_to_actions.split(",")]
-
 
     def set_annotation(self,
         early_time_seconds: int,
@@ -214,29 +154,3 @@ class Configuration:
         self.logo_size_threshold = logo_size_threshold
         self.avg_shot_duration_seconds = avg_shot_duration_seconds
         self.dynamic_cutoff_ms = dynamic_cutoff_ms
-
-
-    def set_model(self,
-        llm_name: str,
-        video_size_limit_mb: int,
-        max_output_tokens: int,
-        temperature: float,
-        top_p: float,
-        top_k: int
-    ) -> None:
-        """Set LLM model parameters.
-
-          Args:
-            llm_name: name of LLm model to use
-            video_size_limit_mb: largest video file to analyze (limit API costs)
-            max_output_tokens: largest response (limit API costs)
-            temperature: how creative the model gets
-            top_p: how varied the model gets
-            top_k: how consistent the model gets
-        """
-        self.llm_name = llm_name
-        self.video_size_limit_mb = video_size_limit_mb
-        self.max_output_tokens = max_output_tokens
-        self.temperature = temperature
-        self.top_p = top_p
-        self.top_k = top_k
