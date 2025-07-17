446935aa7112cbf51d5d823d177a375597c84006
Christoffel <24838075+hiltermann@users.noreply.github.com>
2025-07-17 14:58:03 +0200

    Update generic_helpers.py

--- helpers/generic_helpers.py
+++ helpers/generic_helpers.py
@@ -165,9 +165,7 @@ def print_abcd_assessment(brand_name: str, video_assessment: dict) -> None:
     # Get ABCD evaluations
     if video_assessment.get("annotations_evaluation"):
         print("***** ABCD Assessment using Annotations ***** \n")
-        a = list(video_assessment.get("annotations_evaluation").get("evaluated_features").keys())
-        print(a)
-        # print_score_details(video_assessment.get("annotations_evaluation"))
+        print_score_details(video_assessment.get("annotations_evaluation"))
         
     else:
         print("No annotations_evaluation found. Skipping from priting. \n")
