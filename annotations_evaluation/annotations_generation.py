1c0f2286a5c7b124fd10c12d778a52f18f610cc1
Christoffel <24838075+hiltermann@users.noreply.github.com>
2026-03-05 16:45:50 +0100

    Update annotations_generation.py

--- annotations_evaluation/annotations_generation.py
+++ annotations_evaluation/annotations_generation.py
@@ -24,9 +24,9 @@ import json
 import os
 
 from enum import Enum
-# from google.cloud import videointelligence
+from google.cloud import videointelligence
 from google.cloud.videointelligence import VideoContext
-from google.cloud import videointelligence_v1 as videointelligence
+from google.cloud import videointelligence_v1 as videointelligence2
 from google.cloud.videointelligence_v1 import types
 from google.protobuf.json_format import MessageToDict
 from configuration import Configuration
@@ -97,15 +97,12 @@ def custom_annotations_detection(
     operation = video_client.annotate_video(
         request={
             "features": features,
-            # "input_content": video_blob,
-            "input_uri": "gs://giorgos-4-abcd/bkng/-CwPMSVqwZw.mp4",
+            "input_content": video_blob,
             "video_context": context
         }
     )
     print(f"\nProcessing video for {str(features)} annotations...")
     response = operation.result(timeout=800)
-    print(f"\nOperation: {str(operation)}")
-    # print(f"\nresponse: {str(response)}")
     
     dict_data = MessageToDict(
         response._pb,
@@ -130,7 +127,7 @@ def generate_video_annotations(config: Configuration, video_blob: dict[str, str]
     """Generates video annotations for videos in Google Cloud Storage"""
 
     standard_video_client = videointelligence.VideoIntelligenceServiceClient()
-    # custom_video_client = videointelligence2.VideoIntelligenceServiceClient()
+    custom_video_client = videointelligence2.VideoIntelligenceServiceClient()
 
     # Face Detection
     face_config = videointelligence.FaceDetectionConfig(
@@ -139,12 +136,12 @@ def generate_video_annotations(config: Configuration, video_blob: dict[str, str]
     face_context = videointelligence.VideoContext(face_detection_config=face_config)
 
     # People Detection
-    person_config = types.PersonDetectionConfig(
+    person_config = videointelligence2.types.PersonDetectionConfig(
         include_bounding_boxes=True,
         include_attributes=True,
         include_pose_landmarks=True,
     )
-    person_context = types.VideoContext(
+    person_context = videointelligence2.types.VideoContext(
         person_detection_config=person_config
     )
 
@@ -180,36 +177,36 @@ def generate_video_annotations(config: Configuration, video_blob: dict[str, str]
             standard_video_client, video_blob['blob'], standard_annotations_path, video_path
         ),
     )
-    # tasks.append(
-    #     lambda: custom_annotations_detection(
-    #         standard_video_client,
-    #         face_context,
-    #         [videointelligence.Feature.FACE_DETECTION],
-    #         video_blob['blob'],
-    #         face_annotations_path,
-    #         video_path
-    #     )
-    # )
     tasks.append(
         lambda: custom_annotations_detection(
             standard_video_client,
+            face_context,
+            [videointelligence.Feature.FACE_DETECTION],
+            video_blob['blob'],
+            face_annotations_path,
+            video_path
+        )
+    )
+    tasks.append(
+        lambda: custom_annotations_detection(
+            custom_video_client,
             person_context,
-            [videointelligence.Feature.PERSON_DETECTION],
+            [videointelligence2.Feature.PERSON_DETECTION],
             video_blob['blob'],
             people_annotations_path,
             video_path
         )
     )
-    # tasks.append(
-    #     lambda: custom_annotations_detection(
-    #         standard_video_client,
-    #         speech_context,
-    #         [videointelligence.Feature.SPEECH_TRANSCRIPTION],
-    #         video_blob['blob'],
-    #         speech_annotations_path,
-    #         video_path
-    #     )
-    # )
+    tasks.append(
+        lambda: custom_annotations_detection(
+            standard_video_client,
+            speech_context,
+            [videointelligence.Feature.SPEECH_TRANSCRIPTION],
+            video_blob['blob'],
+            speech_annotations_path,
+            video_path
+        )
+    )
 
     # Execute annotations generation tasks only for the ones that haven't been processed.
     execute_tasks_in_parallel(tasks)
